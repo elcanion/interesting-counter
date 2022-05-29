a1382ff550e80ead09108b279ed33edc7cc6acfb
elcanion <beethovenssixth@gmail.com>
2022-05-29 01:46:44 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import useFetch from 'react-fetch-hook';
 
 function App() {
 
   const [count, setCount] = useState(0)
+  const [fact, setFact] = useState("")
 
-  const {data} = useFetch("http://numbersapi.com/5")
-  console.log(data)
 
   function add(){
     setCount(prevValue => {
       return prevValue + 1
     })
+    var url = `http://numbersapi.com/${count + 1}/trivia`
+    fetch(url, {
+      headers:{
+        'x-requested-with': 'text/plain'
+      }
+    })
+    .then(res => res.text())
+    .then(res => setFact(res))
   }
+
   function subtract(){
     setCount(prevValue => {
       return prevValue - 1
     })
-  }
-  function getFact(){
-    return "teste"
+    var url = `http://numbersapi.com/${count - 1}/trivia`
+    fetch(url, {
+      headers:{
+        'x-requested-with': 'text/plain'
+      }
+    })
+    .then(res => res.text())
+    .then(res => setFact(res))
   }
 
+
   return (
-    <div className="counter">
-      <button className="counter--minus" onClick={subtract}>-</button>
-      <div className="counter--count">
-        <h1>{count}</h1>
+    <div>
+      <h4>interesting counter</h4>
+      <div className="counter">
+          <button className="counter--minus" onClick={subtract}>-</button>
+          <div className="counter--count">
+            <h1>{count}</h1>
+          </div>
+          <button className="counter--plus" onClick={add}>+</button>
+        </div>
+      <div className='fact--box'>
+        <h4 className='fact--text'>{fact}</h4>
       </div>
-      <button className="counter--plus" onClick={add}>+</button>
-      <div className='fact'>{getFact()}</div>
     </div>
   );
 }
