5c3df8c7b9e5e3a4ab1deea030af205c079582c7
elcanion <beethovenssixth@gmail.com>
2022-05-28 22:55:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useFetch from 'react-fetch-hook';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  const {data} = useFetch("http://numbersapi.com/5")
+  console.log(data)
+
+  function add(){
+    setCount(prevValue => {
+      return prevValue + 1
+    })
+  }
+  function subtract(){
+    setCount(prevValue => {
+      return prevValue - 1
+    })
+  }
+  function getFact(){
+    return "teste"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+      <button className="counter--minus" onClick={subtract}>-</button>
+      <div className="counter--count">
+        <h1>{count}</h1>
+      </div>
+      <button className="counter--plus" onClick={add}>+</button>
+      <div className='fact'>{getFact()}</div>
     </div>
   );
 }
